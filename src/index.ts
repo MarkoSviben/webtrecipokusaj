5f3095dc07e7a009a9d881d1756330bed07f9f8f
Marko Sviben <117673654+MarkoSviben@users.noreply.github.com>
2024-10-27 22:31:28 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ dotenv.config();
 
 const main = async () => {
   try {
-    // Provjera veze s bazom podataka
+    
     await prisma.$connect();
     console.log('Uspješno povezan s bazom podataka.');
 
-    // Kreiranje testnog zapisa
+    
     const newTicket = await prisma.ticket.create({
       data: {
         vatin: '12345678901',
@@ -21,7 +21,6 @@ const main = async () => {
       },
     });
 
-    console.log('Kreiran novi tiket:', newTicket);
   } catch (error) {
     console.error('Greška prilikom povezivanja s bazom podataka:', error);
   } finally {
